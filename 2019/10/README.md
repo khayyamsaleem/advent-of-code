8085ccdf509bec7b2a11275f9012f689a802c59a
Khayyam Saleem <hello@khayyam.me>
2020-04-27 14:51:06 -0400

    completes 2019 day 10 part 2

--- 2019/10/README.md
+++ 2019/10/README.md
@@ -26,3 +26,15 @@ cargo run
 ## TODO
 - Learn more about creating crates in rust
 - Separate functions into different files
+- Learn about Rust unit testing
+- Start creating structs and implementing traits ASAP
+    - Ordering would have helped a lot to order the asteroid field
+
+## Notes
+- I was having trouble with arctan and angles at first, which is why there are three distinct angle functions:
+    - `get_angle_between_asteroids(asteroid : Point, other_asteroid : Point) -> (u32, GenericFraction<i32>)`
+    - `get_true_angle_between_asteroids(a : Point, b : Point) -> f64`
+    - `get_legit_angle(Point(x1,y1) : Point, Point(x2,y2) : Point) -> f64`
+
+    This is the greatest shame of my programming career.
+
--- 2019/10/aoc_2019_day_10/src/main.rs
+++ 2019/10/aoc_2019_day_10/src/main.rs
@@ -7,6 +7,7 @@ use std::collections::{ HashSet, HashMap };
 use reqwest::header::{ COOKIE };
 use na::{ DMatrix };
 use fraction::{Sign, GenericFraction};
+use std::f64::consts::{ PI, FRAC_PI_2 };
 
 #[derive(Copy, Clone, Debug, Hash, Eq, PartialEq)]
 struct Point(i32, i32);
@@ -33,7 +34,7 @@ fn get_input(uri : &str) -> Result<DMatrix<char>, Box<dyn std::error::Error>> {
     Ok(input_to_matrix(&res))
 }
 
-fn get_all_asteroid_coords(matrix : DMatrix<char>) -> HashSet<Point> {
+fn get_all_asteroid_coords(matrix : &DMatrix<char>) -> HashSet<Point> {
     let mut asteroids = HashSet::new();
     for x in 0..matrix.ncols() {
         for y in 0..matrix.nrows() {
@@ -85,8 +86,30 @@ fn get_max_entry(visible_counts : HashMap<Point, usize>) -> (Point, usize) {
     max_entry
 }
 
+fn nearly_equal(a: f64, b: f64) -> bool {
+	let abs_a = a.abs();
+	let abs_b = b.abs();
+	let diff = (a - b).abs();
+
+	if a == b { // Handle infinities.
+		true
+	} else if a == 0.0 || b == 0.0 || diff < f64::MIN_POSITIVE {
+		// One of a or b is zero (or both are extremely close to it,) use absolute error.
+		diff < (f64::EPSILON * f64::MIN_POSITIVE)
+	} else { // Use relative error.
+		(diff / f64::min(abs_a + abs_b, f64::MAX)) < f64::EPSILON
+	}
+}
+
+fn get_true_angle_between_asteroids(a : Point, b : Point) -> f64 {
+    let Point(x1, y1) = a;
+    let Point(x2, y2) = b;
+    let ang = ((y2-y1) as f64).atan2((x2-x1) as f64);
+    if ang < 0f64 { 2f64*PI - ang } else { ang }
+}
+
 // part one
-fn get_best_station(asteroid_field : DMatrix<char>) -> (Point, usize) {
+fn get_best_station(asteroid_field : &DMatrix<char>) -> (Point, usize) {
     get_max_entry(
         get_num_visible_for_all_asteroids(
             get_all_asteroid_coords(
@@ -96,10 +119,73 @@ fn get_best_station(asteroid_field : DMatrix<char>) -> (Point, usize) {
     )
 }
 
+fn get_distance_between_asteroids(Point(x1,y1) : Point, Point(x2,y2) : Point) -> i32 {
+    (x2-x1)*(x2-x1) + (y2-y1)*(y2-y1)
+}
+
+fn order_asteroids_by_angle_from_station(station: Point, asteroid_field : &DMatrix<char>) -> Vec<(Point, f64, i32)> {
+    let mut asteroids : Vec<(Point, f64, i32)> = get_all_asteroid_coords(asteroid_field).drain()
+        .map(|p| (p, get_true_angle_between_asteroids(station, p), get_distance_between_asteroids(station, p)))
+        .filter(|(p, _, _)| *p != station)
+        .collect();
+    asteroids.sort_by(|&(_,a1,_),&(_,a2,_)| a1.partial_cmp(&a2).unwrap());
+    asteroids // at this point, cannot guarantee that points along a single ray are ordered by distance
+}
+
+fn group_and_sort_by_angle_from_station(asteroids_and_angles : Vec<(Point, f64, i32)>) -> Vec<(Vec<Point>, f64)> {
+    let mut groups : Vec<(Vec<Point>, f64)> = Vec::new();
+    let mut i = 0;
+    while i < asteroids_and_angles.len() {
+        let mut group = Vec::new();
+        group.push(asteroids_and_angles[i]);
+        if i != asteroids_and_angles.len() - 1 {
+            while nearly_equal(asteroids_and_angles[i].1, asteroids_and_angles[i+1].1) {
+                group.push(asteroids_and_angles[i+1]);
+                i += 1
+            }
+        }
+        group.sort_by(|&(_,_,dist1), &(_,_,dist2)| (dist1).cmp(&dist2));
+        let ang = group[0].1;
+        groups.push((group.iter().map(|(p,_,_)| *p).collect(), ang));
+        i += 1
+    }
+    groups
+}
+
+fn get_legit_angle(Point(x1,y1) : Point, Point(x2,y2) : Point) -> f64 {
+    match ((y2-y1) as f64).atan2((x2-x1) as f64) + FRAC_PI_2 {
+        f if f < 0.0 => f + 2.0*PI,
+        f => f,
+    }
+}
+
+fn zap_asteroids(station : Point, asteroid_field : &DMatrix<char>, stop_after : i64) -> Point {
+    let mut groups : Vec<(Vec<Point>, f64)> = group_and_sort_by_angle_from_station(order_asteroids_by_angle_from_station(station, asteroid_field));
+    groups.sort_by(|(a,_), (b,_)| get_legit_angle(station,a[0]).partial_cmp(&get_legit_angle(station, b[0])).unwrap());
+    let mut group_iterator = 0;
+    let mut num_zapped = 0;
+    loop {
+        if group_iterator == groups.len() {
+            group_iterator = 0
+        }
+        if groups[group_iterator].0.len() > 0 {
+            num_zapped += 1;
+            if num_zapped == stop_after {
+                return groups[group_iterator].0[0]
+            }
+            groups[group_iterator].0.remove(0);
+        }
+        group_iterator += 1;
+    }
+}
+
 fn main() {
     match get_input("https://adventofcode.com/2019/day/10/input") {
         Ok(asteroid_field) => {
-            println!("{:#?}", get_best_station(asteroid_field))
+            let (station, num_visible) = get_best_station(&asteroid_field);
+            println!("Part 1: {:?} => {}", station, num_visible);
+            let Point(x,y) = zap_asteroids(station, &asteroid_field, 200);
+            println!("Part 2: {:?} => {}", Point(x,y), x*100 + y);
         }
         Err(_) => {
             println!("Error")
