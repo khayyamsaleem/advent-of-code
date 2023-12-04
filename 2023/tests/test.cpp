fe5b28ad124c44d4b25b5fc8c022b8f220a8f9d5
ksaleem <ksaleem@digitalocean.com>
2023-12-04 07:29:18 -0500

    2023: day 2 - correct tests

--- 2023/tests/test.cpp
+++ 2023/tests/test.cpp
@@ -74,6 +74,17 @@ Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
 )";
 
   EXPECT_EQ(d2->solve_part1(test_input), 8);
+}
+
+TEST(Day2, Part2) {
+  auto d2 = dynamic_cast<aoc::Day2*>(registry.at(2)().get());
+  auto test_input = R"(Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+)";
+
   EXPECT_EQ(d2->solve_part2(test_input), 2286);
 }
 
